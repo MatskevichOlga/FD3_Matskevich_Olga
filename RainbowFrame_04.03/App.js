58ffc61df4ea12e7b6eaf1a74412926f47c7e9a1
MATSKEVICH_OLGA <Pola221182@list.ru>
2023-03-12 15:08:32 +0300

    12.03

---
+++ RainbowFrame_04.03/App.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import RainbowFrame from './components/RainbowFrame';
+
+
+
+/*
+  render() {
+      let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+      return (
+          <RainbowFrame colors={colors}>
+             <h2>Hello!</h2> 
+          </RainbowFrame>
+      );
+  }*/
+
+  let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+  ReactDOM.render(
+    <RainbowFrame colors={colors}>
+          <h2>Hello!</h2> 
+    </RainbowFrame>
+
+    , document.getElementById('container') 
+  );
+  
+
+
+
+
---
+++ RainbowFrame_04.03/components/RainbowFrame.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+class RainbowFrame extends React.Component {
+  
+ static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+   
+    const cbReduce = (rectangle, current) => {
+      return (
+          <div style={{ border: "solid 6px " + current, padding: "6px" }}>{rectangle}</div>
+      )
+  }
+
+  const rainbowFrameCode = this.props.colors.reduce(cbReduce, this.props.children);
+
+  return (
+      <div style={{ width: 500, textAlign: 'center', margin: '40px auto' }}>{rainbowFrameCode}</div>
+  );
+}
+}
+
+export default RainbowFrame;
+
+/************************* */
+/*
+class RainbowFrame extends React.Component {
+  
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+     let rainbowFrameCode=this.props.children;
+     for (let color of this.props.colors){
+      rainbowFrameCode=<div style={{textAlign: 'center', border:`solid 6px ${color}`, padding: "6px"}}>{rainbowFrameCode}</div>
+     }
+
+
+  return (
+    <div>{rainbowFrameCode}</div>
+  )
+}
+}
+
+
+export default RainbowFrame;*/
