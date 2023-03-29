1e21e0afc4c9d4d35c440365cf77b4fd1a44bb6a
MATSKEVICH_OLGA <Pola221182@list.ru>
2023-03-29 22:37:00 +0300

    second

--- br2jx_04.03/components/BR2JSX.js
+++ br2jx_04.03/components/BR2JSX.js
@@ -1,26 +1,28 @@
 ﻿import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css';
 class BR2JSX extends React.Component {
   
-  static propTypes = {
+ static propTypes = {
     text: PropTypes.string.isRequired,
+   // result:PropTypes.array.isRequired,
 };
   
 render() {
   let lines = this.props.text.split(/<br\s?\/?>/);
-  let result="";
-
+//  let result="";
+let result=[];
   for (let i=0; i<lines.length; i++){
-
-/*    result.push(lines[i]);
+    result.push(lines[i]);
     if(i<(lines.length-1))
-    result.push(<br/>);*/
-    if(i)
-      result+=lines[i];
-    result+=lines[i] + <br/>;
+    result.push(<br/>);
+  
+   // if(i)
+    /*  result+=lines[i];
+    result+=lines[i] + <br/>;*/
 
   }
-return <div>{lines}</div>;
+return <React.Fragment>{result}</React.Fragment>;
 }
 }
 
